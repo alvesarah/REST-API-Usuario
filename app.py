826b70b7906e6722311059f1e0d2b7dfdece726a
alvesarah <sarah.alves8@hotmail.com>
2022-06-19 14:33:44 -0300

    Criando banco de dados automaticamente com SQLAlchemy

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ from flask_restful import Api
 from resources.user import Users, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api =  Api(app)
 
-#Adicionar o recurso. Colocar o nome da classe e da onde quer que seja chamado.
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
+
 api.add_resource(Users, '/users')
 api.add_resource(User, '/users/<int:id>')
 
-#Se o nome for o principal (app.py), rode o arquivo
 if __name__ == '__main__':
-    app.run(debug=True) #Esse debug é True só enquanto estiver em produção
+    from sql_alchemy import banco
+    banco.init_app(app)
+    app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -1,4 +1,13 @@
-class UserModel:
+from sql_alchemy import banco
+class UserModel(banco.Model):
+
+    __tablename__ = 'users'
+
+    id = banco.Column(banco.Integer, primary_key=True, autoincrement=True)
+    nome = banco.Column(banco.String(80))
+    email = banco.Column(banco.String(100))
+    telefone = banco.Column(banco.Integer)
+
     def __init__(self, id, nome, email, telefone):
         self.id = id
         self.nome = nome
