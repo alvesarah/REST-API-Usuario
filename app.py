65eb8896b3bc62a6d1478fec98ce3d670693f8b8
alvesarah <sarah.alves8@hotmail.com>
2022-06-20 04:53:29 -0300

    teste dois

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from blacklist import BLACKLIST
 from resources.user import Users, User, UserPut, UserPost, UserDelete, UserLogin, UserLogout, UserConfirm
 from flask_jwt_extended import JWTManager
-from sql_alchemy import banco
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
@@ -12,7 +12,7 @@ app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
 app.config['JWT_BLACKLIST_ENABLED'] = True
 api =  Api(app)
 jwt = JWTManager(app)
-banco.init_app(app)
+banco = SQLAlchemy(app)
 
 @app.before_first_request
 def cria_banco():
